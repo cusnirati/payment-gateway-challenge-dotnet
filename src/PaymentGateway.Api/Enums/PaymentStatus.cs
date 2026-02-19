1de14e2c86252306f7e6e52c40bdac932c822eb5
ati <cusnirati@gmail.com>
2026-02-19 11:57:38 +0000

    updated description for status returned to merchants

--- src/PaymentGateway.Api/Enums/PaymentStatus.cs
+++ src/PaymentGateway.Api/Enums/PaymentStatus.cs
@@ -2,7 +2,7 @@ namespace PaymentGateway.Api.Models;
 
 public enum PaymentStatus
 {
-    Authorized,
-    Declined,
-    Rejected
+    Authorized, // the payment was authorized by the call to the acquiring bank
+    Declined, // the payment was declined by the call to the acquiring bank
+    Rejected //  No payment could be created as invalid information was supplied to the payment gateway
 }
